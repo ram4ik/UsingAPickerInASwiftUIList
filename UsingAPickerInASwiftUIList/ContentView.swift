ebd0201a9bf3f52f44d6db752ac93884c825b731
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2019-11-12 11:17:34 +0200

    init

--- UsingAPickerInASwiftUIList/ContentView.swift
+++ UsingAPickerInASwiftUIList/ContentView.swift
@@ -9,8 +9,54 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State private var colours = ["red", "orange", "yellow", "green", "blue"]
+    @State private var selection = 4
+    @State private var pickerVisible = false
+    @State private var count = 0
+    
     var body: some View {
-        Text("Hello World")
+        VStack {
+            Text("Picker Tutorial")
+                .font(.largeTitle)
+                .foregroundColor(.gray)
+            List {
+                HStack {
+                    Text("Colour")
+                    Spacer()
+                    Button(colours[selection]) {
+                        if self.pickerVisible {
+                            self.count += 1
+                        }
+                        self.pickerVisible.toggle()
+                    }
+                    .foregroundColor(self.pickerVisible ? .red : .blue)
+                }
+                
+                if pickerVisible {
+                    HStack {
+                        Spacer()
+                        Picker(selection: $selection, label: Text("")) {
+                            ForEach(0..<colours.count) {
+                                Text(self.colours[$0]).foregroundColor(.secondary)
+                            }
+                        }
+                        .onTapGesture {
+                            self.count += 1
+                            self.pickerVisible.toggle()
+                        }
+                        Spacer()
+                    }
+                }
+                
+                HStack {
+                    Text("Number of selections")
+                    Spacer()
+                    Text("\(count)")
+                }
+            }
+            .padding()
+        }
     }
 }
 
